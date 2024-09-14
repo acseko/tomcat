4cc1534cbe25db77d2391709fa811d0ddfe4ee56
András Csekő <acseko@users.noreply.github.com>
2024-09-14 02:19:56 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
-curl "https://repo1.maven.org/maven2/org/apache/tomcat/tomcat/maven-metadata.xml" -O
-
-TOMCAT_VERSION=$(xmlstarlet sel -t -m '//version[starts-with(., "10.1.")]' -v . -n maven-metadata.xml |sort -Vr |head -1)
-rm maven-metadata.xml
+TOMCAT_VERSION="10.1"
 
 for JAVA_MAJOR_VERSION in 11 17 21
   do
     podman build -t tomcat-10:java${JAVA_MAJOR_VERSION} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} --build-arg OPENJDK_MAJOR_VERSION=${JAVA_MAJOR_VERSION} --label openjdk.version=${JAVA_MAJOR_VERSION} --label tomcat.version=${TOMCAT_VERSION} .
 done
-
