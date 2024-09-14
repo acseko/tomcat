22ef754f28ce4f37af379557654ad5c2f456d5de
András Csekő <acseko@users.noreply.github.com>
2024-09-14 21:40:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 TOMCAT_VERSION="10.1"
+RELEASE=$(date +"%YYYY.%mm.%DD-%s")
 
 for JAVA_MAJOR_VERSION in 11 17 21
   do
-    podman build -t tomcat${TOMCAT_VERSION}-java${JAVA_MAJOR_VERSION}:latest --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} --build-arg OPENJDK_MAJOR_VERSION=${JAVA_MAJOR_VERSION} --label openjdk.version=${JAVA_MAJOR_VERSION} --label tomcat.version=${TOMCAT_VERSION} .
+    podman build -t tomcat${TOMCAT_VERSION}-java${JAVA_MAJOR_VERSION}:${RELEASE} --build-arg TOMCAT_VERSION=${TOMCAT_VERSION} --build-arg OPENJDK_MAJOR_VERSION=${JAVA_MAJOR_VERSION} --label openjdk.version=${JAVA_MAJOR_VERSION} --label tomcat.version=${TOMCAT_VERSION} .
 done
