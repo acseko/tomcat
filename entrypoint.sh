32f14207a04893c6a7a889d110262a5d8b803d73
András Csekő <andras.cseko@gmail.com>
2024-09-29 22:52:33 +0200

    Fix on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-export CONNECTOR_PORT=${1:-8080}
-export SHUTDOWN_PORT=$(( ${CONNECTOR_PORT} + 1 ))
-export CONNECTOR=${CONNECTOR:-http}
-echo -e "CONNECTOR_PORT:\t${CONNECTOR_PORT}\nSHUTDOWN_PORT:\t${SHUTDOWN_PORT}\n"
-# ln -sfr /home/tomcat/apache-tomcat/myconf/server.${CONNECTOR}.xml /home/tomcat/apache-tomcat/myconf/server.connector.xml
-# xmlstarlet -e /home/tomcat/apache-tomcat/conf/server.connector.xml
-java -version
-(${CATALINA_HOME}/bin/catalina.sh $*)&
-PID=$!
-echo "waiting for PID to finish: $PID"
-wait ${PID}
+export CATALINA_PID=${CATALINA_HOME}/logs/catalina.pid
+catalina.sh $*
+PID=$(cat ${CATALINA_PID})
+
+echo "Waiting for PID to exit: ${PID}"
+tail --pid=${PID} -f /dev/null
