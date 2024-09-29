8cb3b025b4c97c21595fb2b6845de95d1ec52a26
András Csekő <andras.cseko@gmail.com>
2024-09-29 22:09:23 +0200

    Update entrypoint to wait for finish the background process

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-
-if [ $# -gt 1 ]
-then
-  echo "Update base port number to : $1"
-  shift
-fi;
-${CATALINA_HOME}/bin/catalina.sh $*
+export CONNECTOR_PORT=${1:-8080}
+export SHUTDOWN_PORT=$(( ${CONNECTOR_PORT} + 1 ))
+export CONNECTOR=${CONNECTOR:-http}
+echo -e "CONNECTOR_PORT:\t${CONNECTOR_PORT}\nSHUTDOWN_PORT:\t${SHUTDOWN_PORT}\n"
+# ln -sfr /home/tomcat/apache-tomcat/myconf/server.${CONNECTOR}.xml /home/tomcat/apache-tomcat/myconf/server.connector.xml
+# xmlstarlet -e /home/tomcat/apache-tomcat/conf/server.connector.xml
+java -version
+(${CATALINA_HOME}/bin/catalina.sh $*)&
+PID=$!
+echo "waiting for PID to finish: $PID"
+wait ${PID}
