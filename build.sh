c6c469183b69449957c7be0888032cc58a4d46c2
András Csekő <andras.cseko@gmail.com>
2024-10-23 23:34:04 +0200

    Remove non-required files reported in CVE

--- build.sh
+++
@@ -1,7 +0,0 @@
-TOMCAT_VERSION="10.1"
-RELEASE=$(date +"%Y.%m.%d-%s")
-
-for JAVA_MAJOR_VERSION in 11 17 21
-  do
-    podman build -t tomcat${TOMCAT_VERSION}-java${JAVA_MAJOR_VERSION}:${RELEASE} --build-arg TOMCAT_MAJOR_VERSION=${TOMCAT_VERSION} --build-arg OPENJDK_MAJOR_VERSION=${JAVA_MAJOR_VERSION} --label openjdk.version=${JAVA_MAJOR_VERSION} --label tomcat.version=${TOMCAT_VERSION} .
-done
